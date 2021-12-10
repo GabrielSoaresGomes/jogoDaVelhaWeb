9bf0e55f6e5a2ffc35773c360cf0fcf3ce73c23c
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2021-12-09 23:42:30 -0300

    feat: Lógica pronta e jogo funcional

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ win = [[0,1,2], [3,4,5], [6,7,8],//Horizontais - Horizontals
        [0,3,6], [1,4,7], [2,5,8], //Verticais - Verticals
           [0,4,8], [2,4,6]] //Diagonais - Diagonals
 function insert(clickedField) {
+
     clickedField = parseInt(clickedField)
     let fields = document.getElementsByClassName('field')
     for (let field of fields) {
@@ -29,27 +30,50 @@ function insert(clickedField) {
 }
 var winner = ""
 function checkWin() {
-    let fields = document.getElementsByClassName('field')
+    var winner = ""
     var markedX = []
     var markedCircle = []
+    var xIguais = 0
+    var oIguais = 0
+    let fields = document.getElementsByClassName('field')
     for (var field of fields) {
         if (field.classList.contains('choiceX')) {
             markedX.push(parseInt(field.innerHTML))
         }
-        if (field.classList.contains('choiceCircle')){
+        if (field.classList.contains('choiceCircle')) {
             markedCircle.push(parseInt(field.innerHTML))
             //console.log("markedcir", markedCircle)
         }
     }
     var i = 0
-    while (i < win.length) {
-        console.log("I = "+i+" win "+win[i])
-        if (markedX.includes(win[i])) {
-            winner = "X"
-        }else if (markedCircle.includes(win[i])) {
-            winner = "Circle"
+    for (var w of win) {
+        for (var n of w) {
+            if (markedX.includes(n)) {
+                xIguais++
+            }
+            if (markedCircle.includes(n)) {
+                oIguais++
+            }
+            if (oIguais >= 3 || xIguais >= 3) {
+                break
+            }
         }
-        i++
+        if (oIguais >= 3 || xIguais >=3) {
+                break
+            }
+        oIguais = 0
+        xIguais = 0
+
+
     }
-    console.log(winner)
+    console.log(xIguais)
+    if (xIguais >= 3) {
+        winner = "x"
+        document.getElementsByTagName('h1')[0].innerHTML = `O vencedor foi o Vermelho`
+    }
+    if (oIguais >= 3) {
+        winner = "o"
+        document.getElementsByTagName('h1')[0].innerHTML = `O vencedor foi o Verde`
+    }
+
 }
